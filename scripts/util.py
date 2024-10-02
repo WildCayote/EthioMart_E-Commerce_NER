efcf9cecfc4a4e98567bc60d67cf70eb8ac508fe
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-10-02 20:48:19 +0300

    Feat: Added a function to load conll data

--- scripts/util.py
+++ scripts/util.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def read_conll(path: str):
     """
     A function that will read a CoNLL formated file and return a dataframe from it.
@@ -9,4 +11,34 @@ def read_conll(path: str):
         pandas dataframe of the CoNLL file
     """
 
+    with open(path, 'r' , encoding='utf-8') as file:
+        content = file.read()
     
+    # get all the individual datapoints
+    data_points = content.split('\n\n')
+
+    # get all the words and labels from each data point
+    final_words = []
+    final_labels = []
+    for data_point in data_points:
+        word_label_parrings = data_point.split('\n')
+        words = []
+        lables = []
+        for parring in word_label_parrings:
+            splitting = parring.split(' ')
+            word = splitting[0]
+            lable = splitting[1]
+            words.append(word)
+            lables.append(lable)
+        
+        final_words.append(words)
+        final_labels.append(lables)
+    
+    # create a dataframe
+    result = pd.DataFrame()
+
+    # add the results
+    result['tokens'] = final_words
+    result['lables'] = final_labels
+
+    return result      
