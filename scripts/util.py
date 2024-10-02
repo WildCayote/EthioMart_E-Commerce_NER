8aa4521b3ffc6f35c23ff326f55c6152810a7a10
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-10-02 20:20:52 +0300

    Feat: Added a util module for utility functions

---
+++ scripts/util.py
@@ -0,0 +1,12 @@
+def read_conll(path: str):
+    """
+    A function that will read a CoNLL formated file and return a dataframe from it.
+
+    Args:
+        path(str): the file path of the CoNLL file
+    
+    Returns:
+        pandas dataframe of the CoNLL file
+    """
+
+    
